26f87baa553a05e9ac937b07f41b059965193b27
Samuel <samuel.mohebban@gmail.com>
2020-10-01 15:07:03 -0400

    removing some emotion categories

--- PyFunctions/Functions.py
+++ PyFunctions/Functions.py
@@ -27,34 +27,33 @@ def get_emotion_classes(class_type, max_values = 4500):
     angry_paths = [f'../EmotionDataset/{class_type}/angry/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/angry')][:max_values]
     angry_labels = [0 for i in range(len(angry_paths))]
     
-    disgust_paths = [f'../EmotionDataset/{class_type}/disgust/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/disgust')][:max_values]
-    
-    disgust_labels = [1 for i in range(len(disgust_paths))]
-    
     happy_paths = [f'../EmotionDataset/{class_type}/happy/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/happy')][:max_values] 
-    happy_labels = [2 for i in range(len(happy_paths))]
+    happy_labels = [1 for i in range(len(happy_paths))]
     
     neutral_paths = [f'../EmotionDataset/{class_type}/neutral/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/neutral')][:max_values] 
     
-    neutral_labels = [3 for i in range(len(neutral_paths))]
+    neutral_labels = [2 for i in range(len(neutral_paths))]
     
     sad_paths = [f'../EmotionDataset/{class_type}/sad/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/sad')][:max_values] 
-    sad_labels = [4 for i in range(len(sad_paths))]
+    sad_labels = [3 for i in range(len(sad_paths))]
     
+#     disgust_paths = [f'../EmotionDataset/{class_type}/disgust/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/disgust')]
+#     disgust_labels = [4 for i in range(len(disgust_paths))]
+
 #     fear_paths = [f'../EmotionDataset/{class_type}/fear/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/fear')]
 #     fear_labels = [5 for i in range(len(fear_paths))]
     
 #     surprise_paths = [f'../EmotionDataset/{class_type}/surprise/{i}' for i in os.listdir(f'../EmotionDataset/{class_type}/surprise')] 
 #     surprise_labels = [6 for i in range(len(surprise_paths))]
     
-    labels = np.array(angry_labels + disgust_labels + happy_labels + \
+    labels = np.array(angry_labels + happy_labels + \
                         neutral_labels + sad_labels)
     
     print(f'{class_type.upper()} Value Count')
     print(pd.Series(labels).value_counts())
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     labels = to_categorical(labels)
-    paths = np.array(angry_paths + disgust_paths + happy_paths + neutral_paths + \
+    paths = np.array(angry_paths + happy_paths + neutral_paths + \
                           sad_paths)
     paths, labels = sk_shuffle(paths, labels)
     return paths, labels
